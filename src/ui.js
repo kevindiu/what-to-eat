479a78d9cff3d42ef79247880054df1ac827b777
kevindiu <kevindiujp@gmail.com>
2025-12-30 09:37:26 +0900

    fix

--- src/ui.js
+++ src/ui.js
@@ -394,6 +394,30 @@ export const UI = {
                 bounds.extend(place.location);
                 bounds.extend(App.Data.userPos);
                 map.fitBounds(bounds, 50);
+
+                // Add Re-center Control
+                const centerBtn = document.createElement('button');
+                centerBtn.className = 'map-center-btn';
+                centerBtn.innerHTML = '<svg viewBox="0 0 24 24" width="22" height="22" fill="#666"><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>';
+                centerBtn.title = 'Re-center Map';
+                centerBtn.onclick = () => {
+                    const b = new google.maps.LatLngBounds();
+                    b.extend(place.location);
+                    b.extend(App.Data.userPos);
+                    map.fitBounds(b, 50);
+                };
+                map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerBtn);
+            } else {
+                // If no user position, just center on place
+                const centerBtn = document.createElement('button');
+                centerBtn.className = 'map-center-btn';
+                centerBtn.innerHTML = '<svg viewBox="0 0 24 24" width="22" height="22" fill="#666"><path d="M12 8c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm8.94 3c-.46-4.17-3.77-7.48-7.94-7.94V1h-2v2.06C6.83 3.52 3.52 6.83 3.06 11H1v2h2.06c.46 4.17 3.77 7.48 7.94 7.94V23h2v-2.06c4.17-.46 7.48-3.77 7.94-7.94H23v-2h-2.06zM12 19c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7z"/></svg>';
+                centerBtn.title = 'Re-center Map';
+                centerBtn.onclick = () => {
+                    map.setCenter(place.location);
+                    map.setZoom(16);
+                };
+                map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(centerBtn);
             }
 
             google.maps.event.addListenerOnce(map, 'idle', () => container.classList.remove('skeleton'));
